141b7a25ced48965c21537d1bc752f71a4b5b885
a.ankit <a.ankit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-09 11:26:30 +0000

    First commit 9th october 2013
    git-svn-id: https://plugins.svn.wordpress.org/easy-coming-soon/trunk@785182 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ coming-soon-plugin.php
@@ -0,0 +1,426 @@
+<?php
+					/*
+					Plugin Name:Easy Coming Soon
+					Description: Creates a Coming Soon page for your Website while Coming soon mode on.Collects E-mails and connect with Socials links.
+					Version:1.0
+					Author:Deepesh paiwal
+					Author URI: 
+					Plugin URI: 
+					*/
+
+		add_action('admin_menu','coming_soon_menu');
+
+
+        /*
+        * Coming Soon menu
+        *
+        * for create plugin menu on admin panel
+        *
+        */
+		function coming_soon_menu()
+		{
+        
+		 	$menu = add_menu_page('Easy coming soon', 'Easy coming soon','administrator', 'coming_soon',	'construction');
+	
+			add_action( 'admin_print_styles-' . $menu, 'coming_soon_plugin_js_css' );
+		}
+
+        /*
+        * Coming Soon plugin js and css
+        *
+        * attach file css and js from plugin directory
+        *
+        */
+		
+
+		function coming_soon_plugin_js_css()
+		{
+            // enqueue script for coming soon plugin
+			
+			wp_enqueue_script('dashboard');
+			
+			wp_enqueue_script( 'theme-preview' );
+		    wp_enqueue_script('tab',plugins_url('/js/spa-tab.js',__FILE__));
+		    wp_enqueue_script('tab-content',plugins_url('/js/jquery.option.tree.js',__FILE__));
+		    wp_enqueue_script('media-uploads',plugins_url('/js/media-upload-script.js',__FILE__),array('media-upload','thickbox','jquery'));
+			
+		   
+		   
+  
+			wp_enqueue_style('dashboard');
+            //enqueue css for coming soon plugin
+			wp_enqueue_style('spa_flexslider', plugins_url('/css/theme-admin.css',__FILE__));
+            wp_enqueue_style('classic',plugins_url('/css/style-classic.css',__FILE__));
+            wp_enqueue_style('option',plugins_url('/css/style-option.css',__FILE__));
+            wp_enqueue_style('thickbox');
+            wp_enqueue_style('farbtasticss',plugins_url('/css/farbtasticss.css',__FILE__));
+            
+			
+        }
+		
+		add_action('admin_bar_menu', 'my_custom_menu', 1000);
+function my_custom_menu()
+{
+    global $wp_admin_bar;
+	$value=get_option('soon_page_settings');
+    if($value['status']=='0') return;
+    // Add Parent Menu
+    $argsParent=array(
+        'id' => 'myCustomMenu',
+        'title' => 'Coming Soon Mode Enable',
+        'href' => '?page=coming_soon',
+    );
+    $wp_admin_bar->add_menu($argsParent);
+
+   
+}
+
+        /*
+        * Construction
+        *
+        * This function is render all setting page on coming soon plugin page
+        *
+        */
+        function construction()
+		{   ?>
+        
+		    <!-- div "wrap" is start to get all setting page in warpper  -->
+            <div class="wrap" id="framework_wrap">
+
+                <!-- div class icon32 is start to attach coming soon icon on top page heading -->
+                <div class="icon32" id="icon-options-general">
+                </div>
+                <!-- div class icon32 is close  -->
+
+                <!-- coming soon plugin headline -->
+                <h2 style="color:#21759b;background:#C3D9FF;width:180%"><?php _e('Coming Soon Plugin');?></h2>
+                <br />
+                <div class="one-third">
+                
+                    <!--div content_wrap is start -->
+                    <div id="content_wrap">
+                        <!-- div content is start to add content in tabs-->
+                        <div id="content">
+                            <!-- div options_tabs is start to add tab list in left side of warpper -->
+                            <div id="options_tabs" class="ui-tabs ">
+                                <ul class="options_tabs ui-tabs-nav" role="tablist">
+                                    <li class="active" role="tab" tabindex="0" aria-controls="option_general_default" aria-labelledby="ui-id-1" aria-selected="true"><a href="#" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-1"><img src="<?php echo plugins_url('images/icon_edit.png',__FILE__)?>" />&nbsp;<?php _e('General');?></a><span></span>
+                                    </li>
+
+                                    <li  role="tab" tabindex="-1" aria-controls="option_demo_header" aria-labelledby="ui-id-2" aria-selected="true">
+                                    <a href="#" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-2"><img src="<?php echo plugins_url('images/icon_classic.png',__FILE__)?>" />&nbsp;<?php _e('Design');?></a><span></span>
+                                    </li>
+                                     <li role="tab" tabindex="-1" aria-controls="option_color_header" aria-labelledby="ui-id-5" aria-selected="false"><a href="#" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-5"><img src="<?php echo plugins_url('images/message.png',__FILE__)?>" />&nbsp;<?php _e('Notification');?></a><span></span>
+                                     </li>
+
+                                   <li><a href="<?php echo home_url(); ?>?my_preview=true&TB_iframe=true&width=500&height=532" title="<?php _e('Live Preview Window') ?>"  class="ui-tabs-anchor thickbox-preview"  ><img src="<?php echo plugins_url('images/icon_on.png',__FILE__)?>" />&nbsp;<?php _e('Live Preview');?> </a>
+                                    </li>
+                                </ul>
+
+                                <!-- Apply script for call open window on live preview button for preview coming soon page-->
+                                <!-- tab-1 to add general settings on right side of coming soon plugin wrapper -->
+                                <?php require_once('general-settings.php');?>
+                                <!-- tab-2 to add design settings on right side of coming soon plugin wrappers-->
+                                <?php include('design-settings.php');?>
+                                <!-- tab-3 to add notification settings on right side of coming soon plugin wrappers-->
+                                <?php include('notification-settings.php');?>
+
+                                <br class="clear">
+                            </div><!-- end div of option_tabs -->
+                        </div><!-- end div of content -->
+                    </div><!-- end div of content_wrap -->
+		        </div> <!-- end of div one third-->
+            </div> <!-- end of div wrap -->
+                <!--One Forth Part Right End-->
+            <?php
+		}
+
+        /*
+        * add_action to call coming_soon_general_page_save_settings
+        */
+
+		add_action('wp_ajax_coming_soon_page_settings', 'coming_soon_general_page_save_settings');
+
+        /*
+        * Function coming_soon_general_page_save_settings function
+        *
+        * To get data from general setting page after  "save settings" by ajax post
+        *
+        */
+
+		function coming_soon_general_page_save_settings()
+		{
+            //when ajax action call to post data from general settings page
+		    if(isset($_POST['action']) == "coming_soon_page_settings")
+		    {
+		        $status = mysql_real_escape_string($_POST['status']);
+		        $background = mysql_real_escape_string($_POST['background']);
+		        $title = mysql_real_escape_string($_POST['title']);
+		        $descrip = mysql_real_escape_string($_POST['descrip']);
+				$google_code = stripslashes($_POST['google_code']);
+		        
+				
+		        $fb = mysql_real_escape_string($_POST['fb']);
+		        $twit = mysql_real_escape_string($_POST['twit']);
+		        $gp = mysql_real_escape_string($_POST['gp']);
+
+                //all data is sotre in array form
+		        $general_page_save_setting = array( 'status' => $status,
+		        'background' => $background,
+		        'title' => $title,
+		        'descrip' => $descrip,
+				'google_code' => $google_code,
+		        
+				
+		        'fb' => $fb,
+		        'twit' => $twit,
+		        'gp' => $gp
+		        );
+		        // data is insert into option table
+		        update_option("soon_page_settings", $general_page_save_setting);
+		    }
+		}
+
+        /*
+        * add_action to call coming_soon_general_page_reset_settings
+        */
+
+        add_action('wp_ajax_coming_soon_page_reset_settings', 'coming_soon_general_page_reset_settings');
+
+        /*
+        * Function coming_soon_general_page_reset_settings 
+        *
+        * To get data from general setting page after  "reset data" by ajax post
+        *
+        */
+		function coming_soon_general_page_reset_settings()
+		{
+		    if(isset($_POST['action']) == "coming_soon_page_reset_settings")
+		    {
+		        $status = "1";
+		        $background = "";
+		        $title = "";
+		        $descrip = "";
+		        
+		        $fb = "";
+		        $twit ="";
+		        $gp = "";
+		
+                $general_page_reset_setting = array( 'status' => $status,
+		        'background' => $background,
+		        'title' => $title,
+		        'descrip' => $descrip,
+		        
+		        'fb' => $fb,
+		        'twit' => $twit,
+		        'gp' => $gp
+		        );
+                //send data into option table by update option
+		        update_option("soon_page_settings", $general_page_reset_setting);
+		    }
+		}
+
+        /*
+        *  add_action to call coming_soon_design_page_save_settings function
+        */
+		add_action('wp_ajax_coming_soon_page_design_settings', 'coming_soon_page_design_save_settings');
+
+        /*
+        * Function coming_soon_design_page_save_settings
+        *
+        * To get data from design setting page after  "save settings" by ajax post
+        *
+        */
+		function coming_soon_page_design_save_settings()
+		{
+		    if(isset($_POST['action']) == "coming_soon_page_design_settings")
+		    {
+		        $background_color = mysql_real_escape_string($_POST['background_color']);
+		        $title_color = mysql_real_escape_string($_POST['title_color']);
+		        $title_font_size = mysql_real_escape_string($_POST['pxselect_post_title']);
+		        $title_font_format = mysql_real_escape_string($_POST['font_family_post_title']);
+                $descrip_color = mysql_real_escape_string($_POST['descrip_color']);
+		        $description_font_size = mysql_real_escape_string($_POST['pxselect_post_entry']);
+		        $description_font_format = mysql_real_escape_string($_POST['font_family_post_description']);
+		
+                $design_settings_save_setting = array( 'background_color' => $background_color,
+		        'title_color' => $title_color,
+		        'title_font_size' => $title_font_size,
+		        'title_font_format' => $title_font_format,
+                'descrip_color' => $descrip_color,
+		        'description_font_size' => $description_font_size,
+		        'description_font_format' => $description_font_format,
+                );
+
+		        update_option("soon_page_desgin_settings", $design_settings_save_setting);
+		    }
+		}
+
+        /*
+        * add_action to call coming_soon_design_page_reset_settings function
+        */
+        add_action('wp_ajax_coming_soon_page_design_reset_settings', 'coming_soon_page_design_reset_settings');
+
+        /*
+        * Function coming_soon_design_page_reset_settings
+        *
+        * To get data from design setting page after  "reset settings" by ajax post
+        *
+        */
+		function coming_soon_page_design_reset_settings()
+		{
+	        if(isset($_POST['action']) == "coming_soon_page_design_reset_settings")
+		    {
+		        $background_color ="#21759B";
+		        $title_color = "#ffffff";
+		        $descrip_color ="#ffffff";
+		        $title_font_size = "30";
+		        $title_font_format = "Arial Black, sans-serif";
+		        $description_font_size ="20";
+		        $description_font_format = "Verdana, Geneva, sans-serif";
+
+                $design_page_reset_setting = array( 'background_color' => $background_color,
+		        'title_color' => $title_color,
+		        'descrip_color' => $descrip_color,
+		        'title_font_size' => $title_font_size,
+		        'title_font_format' => $title_font_format,
+		        'description_font_size' => $description_font_size,
+		        'description_font_format' => $description_font_format,
+		    	);
+		
+		        update_option("soon_page_desgin_settings", $design_page_reset_setting);
+		    }
+		}
+/*
+        * add_action to call coming_soon_page_notification_settings function
+        */
+        add_action('wp_ajax_coming_soon_page_notification_settings', 'coming_soon_page_notification_settings');
+		/*
+        * Function coming_soon_Notification_page_save_settings
+        *
+        * To get data from notification setting page after  "save settings" by ajax post
+        *
+        */
+		function coming_soon_page_notification_settings()
+		{
+		     if(isset($_POST['action']) == "coming_soon_page_notification_settings")
+		    {
+		        	$mailto = ($_POST['mailto']);
+				$email_address = ($_POST['email_address']);
+				$message = ($_POST['message']);
+				$sb_btn = ($_POST['sb_btn']);
+		        
+
+                //all data is store in array form
+		        $notification_page_save_setting = array( 
+		        
+				'mailto' => $mailto,
+				'email_address' => $email_address,
+				'message' => $message,
+				'sb_btn' => $sb_btn,
+		       
+		        );
+		        // data is insert into option table
+		        update_option("soon_page_notification_settings", $notification_page_save_setting);
+		        
+			
+		    }
+		}
+		
+		
+		/*
+        * add_action to call coming_soon_page_reset_notification_settings function
+        */
+        add_action('wp_ajax_coming_soon_page_reset_notification_settings', 'coming_soon_page_reset_notification_settings');
+		/*
+        * Function coming_soon_Notification_page_save_settings
+        *
+        * To get data from notification setting page after  "reset settings" by ajax post
+        *
+        */
+		function coming_soon_page_reset_notification_settings()
+		{
+		     if(isset($_POST['action']) == "coming_soon_page_reset_notification_settings")
+		    {
+		        
+		       	$mailto = "no";
+				$email_address ="";
+				$sb_btn ="Notify Me!";
+				
+		        
+
+                //all data is sotre in array form
+		        $notification_page_reset_setting = array( 
+		        
+				'mailto' => $mailto,
+				'email_address' => $email_address,
+				'sb_btn' => $sb_btn,
+		       
+		        );
+		        // data is insert into option table
+		        update_option("soon_page_notification_settings", $notification_page_reset_setting);
+			
+		    }
+		}
+        /*
+        * Function  my_page_template_redirect
+        *
+        * To redirect our site url by coming soon page,after redirecting only our coming soon page can display
+        *
+        */
+        function my_page_template_redirect()
+        {
+            //get status value from option table for enable or disable coming soon page
+	        $value=get_option('soon_page_settings');
+
+            //condition matching if status is not disable for coming soon then page is redirect
+			if($value['status']!=='0')
+            {
+				if(!is_feed())
+				{
+                	//if user not login page is redirect on coming soon template page
+	            	if ( !is_user_logged_in()  )
+                	{
+                        //get path of our coming soon display page and redirecting
+                    	$file = plugin_dir_path(__FILE__).'template/coming_soon_display.php';
+	                	include($file);
+			        	exit();
+                	}
+				}
+                //if user is log-in then we check role.
+	            if (is_user_logged_in() )
+                {
+                    //get logined in user role
+		            global $current_user;
+		            get_currentuserinfo();
+		            $LoggedInUserID = $current_user->ID;
+		            $UserData = get_userdata( $LoggedInUserID );
+		            //if user role not 'administrator' redirect him to message page
+		            if($UserData->roles[0] != "administrator")
+		            {
+                        if(!is_feed())
+                        {
+		                    $file = plugin_dir_path(__FILE__).'template/coming_soon_display.php';
+	                       include($file);
+			               exit();
+		                }
+                    }
+		        }
+            }
+        }
+
+        /*
+        * add action to call function my_page_template_redirect
+        */
+        add_action( 'template_redirect', 'my_page_template_redirect' );
+		
+		        //get page for live preview when my_preview is true,that mean when this page is call on live preview
+                if (  (isset($_GET['my_preview']) && $_GET['my_preview'] == 'true') )
+                {
+                    $file = plugin_dir_path(__FILE__).'template/coming_soon_display.php';
+	                include($file);
+			        exit();
+                }
+		 
+		 
+?>
---
+++ readme.txt
@@ -0,0 +1,58 @@
+=== Easy Coming Soon ===
+Contributors: a.ankit, deepeshpaliwal
+Donate link: http://www.webriti.com/
+Tags: coming soon, wordpress coming soon, wordpress under construction, wordpress maintenance mode, maintenance mode, under construction, coming soon page, launch page, maintenance, construction, offline,landing page 
+Requires at least: 3.3+
+Tested up to: 3.6.1
+Stable tag: 0.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easy coming Soon plugin allows you to quickly create a launch / under construction page for your website. Collects E-mails and connect with users on Social Networks. 
+
+== Description ==
+
+The **Easy Coming Soon** plugin allows you quckly create a Launch page / Coming Soon page for your wordpress website.  Simply activate the plugin, Setup Page Title , Description and you are ready to go. 
+
+The Easy Coming Soon plugin works with any WordPress theme you have installed on your site. Looged out users will see the coming soon page while logged-in users will have access to the website. This is ideal for web developers who want to present their clients with a Under construction page while working on the website. 
+
+Give **Easy Coming Soon** a try. We are sure you will like it. 
+
+In case you face any problem, contact us via the [Forums](http://wordpress.org/support/plugin/easy-coming-soon). 
+
+
+**Features**
+
+* Works with any WordPress Theme
+* Responsive
+* Provide Social connectivity(Facebook, Twitter, Google+)
+* Free Coming soon page template
+* Flexible and user-friendly setup
+* Live Preview of coming soon page
+* Subscribe feature / Easily collect visitor emails
+* Add Google Analytics Tracking to the Coming Soon Page
+* Very easy customization of coming soon page template setup
+
+
+== Installation ==
+
+1. Download Easy coming soon plugin.
+2. Upload the easy-coming-soon folder to the /wp-content/plugins/ directory.
+3. Activate the plugin through the 'Plugins' menu in WordPress and Enjoy.
+
+
+== Frequently Asked Questions ==
+
+
+
+
+== Screenshots ==
+
+1. Create Coming soon page like this
+2. Coming soon plugin general setting panel
+3. Coming soon plugin Design panel
+
+== Changelog ==
+
+= 0.5 =
+This version provides basic functionality to craete easily coming soon or launching page and get e-mail on wordpress blog.
